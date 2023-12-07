c3ea1530e2a4c97556f4289626ad30faabd4b280
royamousavi <roya2002_rm@yahoo.com>
2023-12-07 21:05:09 +0200

    ok

--- src/component/country.jsx
+++ src/component/country.jsx
@@ -3,13 +3,15 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 
-
+const api_key = import.meta.env.VITE_SOME_KEY;
 
 function Country({ data: { name, capital, population, flags, languages } }) {
   const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     if (capital) {
-      const api_key = import.meta.env.VITE_SOME_KEY;
       axios
         .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`)
         .then((response) => {
